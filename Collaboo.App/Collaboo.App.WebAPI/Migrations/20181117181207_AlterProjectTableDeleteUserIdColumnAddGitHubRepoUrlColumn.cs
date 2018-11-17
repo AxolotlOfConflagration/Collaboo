5af3f9addc8f8bb2250157a8dbf6cfbb297e1182
msciborski <michal.sciborski@student.put.poznan.pl>
2018-11-17 19:14:02 +0100

    Delete UserId prop from Project, Add GitHubRepoUrl to Project

---
+++ Collaboo.App/Collaboo.App.WebAPI/Migrations/20181117181207_AlterProjectTableDeleteUserIdColumnAddGitHubRepoUrlColumn.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Collaboo.App.WebAPI.Migrations
+{
+    public partial class AlterProjectTableDeleteUserIdColumnAddGitHubRepoUrlColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Projects");
+
+            migrationBuilder.AddColumn<string>(
+                name: "GitHubRepoUrl",
+                table: "Projects",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GitHubRepoUrl",
+                table: "Projects");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Projects",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
