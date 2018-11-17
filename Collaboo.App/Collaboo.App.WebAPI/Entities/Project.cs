eec41052e1f62f3c6d6284ad6516db8018d56356
msciborski <michal.sciborski@student.put.poznan.pl>
2018-11-17 19:14:02 +0100

    Delete UserId prop from Project, Add GitHubRepoUrl to Project

--- Collaboo.App/Collaboo.App.WebAPI/Entities/Project.cs
+++ Collaboo.App/Collaboo.App.WebAPI/Entities/Project.cs
@@ -8,7 +8,7 @@ namespace Collaboo.App.WebAPI.Entities
         public string ProjectName { get; set; }
         public int OwnerId { get; set; }
         public string Description { get; set; }
-        public int UserId { get; set; }
+        public string GitHubRepoUrl { get; set; }
         public ICollection<ProjectRequirement> Requirements { get; set; }
         public ICollection<ProjectMember> ProjectMembers { get; set; }
     }
