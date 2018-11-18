04515de3d5ad41d84475abd2f132c85151c32133
Bartosz Mikulski <bartmiki97@gmail.com>
2018-11-18 01:43:32 +0100

    Recommendation Client implemented

---
+++ Collaboo.App/Collaboo.App.WebAPI/Controllers/RecommendationController.cs
@@ -0,0 +1,20 @@
+﻿using Collaboo.App.Recommendation;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Collaboo.App.WebAPI.Controllers
+{
+    [Route("api/recommendation")]
+    public class RecommendationController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> MostPopularLanguages()
+        {
+            var clinet = new RecommendationClient();
+            return Ok(await clinet.MostPopularLanguages());
+        }
+    }
+}
