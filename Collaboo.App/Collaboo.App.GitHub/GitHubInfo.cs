18a70de02f4fa99ebc7a1520ac4e3a3dd6a7ddde
msciborski <michal.sciborski@student.put.poznan.pl>
2018-11-18 01:44:36 +0100

    Adding GitHubInfo

---
+++ Collaboo.App/Collaboo.App.GitHub/GitHubInfo.cs
@@ -0,0 +1,9 @@
+using Octokit;
+
+namespace Collaboo.App.GitHub
+{
+    public class GitHubInfo
+    {
+        public void GetCommitsFromRepo(string repoName, string gitHubLogin)
+        {
+        }
