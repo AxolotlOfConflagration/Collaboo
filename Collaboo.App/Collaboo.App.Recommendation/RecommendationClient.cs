9a2bcd42d20d372645154b541a0098904ea7ae4c
Bartosz Mikulski <bartmiki97@gmail.com>
2018-11-18 01:16:22 +0100

    Recomendationclient

--- Collaboo.App/Collaboo.App.Recommendation/RecommendationClient.cs
+++ Collaboo.App/Collaboo.App.Recommendation/RecommendationClient.cs
@@ -1,7 +1,44 @@
+using Collaboo.App.Recommendation.Helpers;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
 namespace Collaboo.App.Recommendation
 {
     public class RecommendationClient
     {
-        
+        private readonly Uri _recomendationServerUri = new Uri(@"https://gentle-bastion-31804.herokuapp.com");
+        private readonly Dictionary<string, string> _endpoints;
+
+        public string MostPopularLanguagesEndpoint { get; } = "mpl";
+        public string ProjectPerUserEndpoint { get; } = "ppu";
+        public string ProjcetsForUserEndpoint { get; } = "pfu";
+        public string UserForProjcetsEndpoint { get; } = "ufp";
+
+        public RecommendationClient()
+        {
+            _endpoints = new Dictionary<string, string>
+            {
+                {MostPopularLanguagesEndpoint, "mpl" },
+                {ProjectPerUserEndpoint, "ppu/{0}" },
+                {ProjcetsForUserEndpoint, "usp/u/{0}" },
+                {UserForProjcetsEndpoint, "usp/p/{0}" }
+            };
+        }
+
+        private string GetEndpoint(string endpointName, params object[] args)
+        {
+            return string.Format($"{_recomendationServerUri}/{_endpoints[endpointName]}", args);
+        }
+
+        public async void MostPopularLanguages()
+        {
+            var request = GetEndpoint(MostPopularLanguagesEndpoint);
+            var result = await HttpRequestFactory.Get(request);
+
+            var content = await result.Content.ReadAsStringAsync();
+
+            var json = JArray.Parse(content);
+        }
     }
 }
