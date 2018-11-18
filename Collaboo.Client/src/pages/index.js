f16c6a12367ef8c828f0ba22de833d7b44b66dac
Norbert Kaźmierczak <norbini12@gmail.com>
2018-11-18 03:03:39 +0100

    Add reading user projects

--- Collaboo.Client/src/pages/index.js
+++ Collaboo.Client/src/pages/index.js
@@ -8,9 +8,10 @@ import Feed from '../components/feed/feed';
 import Sidebar from '../components/sidebar';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { CreateProject } from './create-project';
+import { Login } from './login';
 import { Provider } from 'react-redux';
 import { store } from '../reducers/store';
-import classNames from 'classnames';
+import { getUser } from '../services';
 
 const styles = theme => ({
   root: {
@@ -40,21 +41,51 @@ class Index extends React.Component {
     open: false,
   };
 
+  componentDidMount(){
+    let user = getUser();
+    if(!user) console.log(user);
+  }
+
   handleClose = () => {
     this.setState({
       open: false,
     });
   };
 
+
   handleClick = () => {
     this.setState({
       open: true,
     });
   };
+  
+  findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    window.location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+  }
 
   render() {
     const { classes } = this.props;
+    let appState = store.getState();
     
+    let loggedIn = localStorage.getItem('user');
+    if(window.location.href.indexOf("authorize") > -1) {
+      let userId = this.findGetParameter('id');
+      let user = getUser(userId).then(data => {
+        localStorage.setItem('user', JSON.stringify(data));
+      });
+    }
+    
+
+
     const Users = () => <h2>Users</h2>;
 
     return (
@@ -65,15 +96,19 @@ class Index extends React.Component {
         <br />
         <Grid container spacing={24} className={classes.container}>
             <Grid item md={3}> 
+            {loggedIn &&
               <Sidebar />
+            }
             </Grid>
             <Grid item md={6}>
               <Route path="/" exact component={Feed} />
               <Route path="/new-project" component={CreateProject} />
-              <Route path="/users/" component={Users} />
+              <Route path="/login" component={Login} />
             </Grid>
             <Grid item md={3}> 
-              Users activity or sth
+            {loggedIn &&
+              <p>Users activity or sth</p>
+            }
             </Grid>
         </Grid>
         
--- Collaboo.Client/src/services/index.js
+++ Collaboo.Client/src/services/index.js
@@ -1,5 +1,8 @@
 const apiUrl = 'https://collabooappwebapi20181117101038.azurewebsites.net/api/'
 
+
+
+
 export const createProject = function(project) {
     const requestOptions = {
         method: 'POST',
@@ -8,14 +11,56 @@ export const createProject = function(project) {
         },
         body: JSON.stringify(project)
     };
-    console.log(JSON.stringify(project))
-    let result = fetch(apiUrl + 'users/3/projects', requestOptions)
+    fetch(apiUrl + 'users/3/projects', requestOptions)
+    .then((response) => {
+        return response;
+    });
+}
+export const login = function() {
+    const requestOptions = {
+        method: 'GET'
+    };
+    fetch(apiUrl + 'login', requestOptions)
     .then((response) => {
         return response;
     });
 
-    console.log(result);
+}
 
-    return result;
+export const getUser = function(id) {   
+    return fetch(apiUrl + 'users/' + id)
+    .then(handleResponse)
+    .then(data => {
+        return data;
+    })
 
+}
+
+export const getProjects = function() {
+
+    let user = JSON.parse(localStorage.getItem('user'));
+    console.log("User: ", user)
+    return fetch(apiUrl + 'users/' + user.id + '/projects')
+    .then(handleResponse)
+    .then(data => {
+    return data;
+    })
+
+}
+
+function handleResponse(response) {
+    return new Promise((resolve, reject) => {
+        if (response.ok) {
+            // return json if it was returned in the response
+            var contentType = response.headers.get("content-type");
+            if (contentType && contentType.includes("application/json")) {
+                response.json().then(json => resolve(json));
+            } else {
+                resolve();
+            }
+        } else {
+            // return error message from response body
+            reject(response.statusText, response.status);
+        }
+    });
 }
