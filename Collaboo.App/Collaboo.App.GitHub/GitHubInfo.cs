2e2e6996c33084d7e0a21b6c2e1f51ef6ce223b6
Bartosz Mikulski <bartmiki97@gmail.com>
2018-11-18 01:55:15 +0100

    GetRepos

--- Collaboo.App/Collaboo.App.GitHub/GitHubInfo.cs
+++ Collaboo.App/Collaboo.App.GitHub/GitHubInfo.cs
@@ -1,9 +1,4 @@
-using Octokit;
 
 namespace Collaboo.App.GitHub
 {
-    public class GitHubInfo
-    {
-        public void GetCommitsFromRepo(string repoName, string gitHubLogin)
-        {
-        }
+}
